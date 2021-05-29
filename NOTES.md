b2832206e8837e865daf7a3d6793bab0a3e85078
David Duarte <davidandradeduarte@gmail.com>
2021-05-29 17:42:46 +0100

    adding decimal to base

--- NOTES.md
+++ NOTES.md
@@ -7,3 +7,8 @@
     sb.WriteString(" World!")
     fmt.Println(sb.String())
     ```
+- binary is base 2; decimal is base 10; hex is base 16
+- the maximum number of a base system is n-1; so in binary the maximum number is 1 (2-1)
+- in hex we represent numbers greater than 9 with letters. e.g 10=A, 11=B, 12=C ... 15=F, because 15 (16-1) is the maximum number of base 16
+- little endian/big endian https://learnmeabitcoin.com/technical/little-endian
+- 
--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -1,5 +1,7 @@
 package module01
 
+import "fmt"
+
 // DecToBase will return a string representing
 // the provided decimal number in the provided base.
 // This is limited to bases 2-16 for simplicity.
@@ -10,5 +12,43 @@ package module01
 //   DecToBase(14, 2) => "1110"
 //
 func DecToBase(dec, base int) string {
-	return ""
+	if base > 16 {
+		panic("base not supported")
+	}
+
+	result := ""
+	for i := dec; i != 0; {
+		if base > 10 {
+			result = DecToHex(i%base) + result
+		} else {
+			result = fmt.Sprintf("%d", i%base) + result
+		}
+
+		i = i / base
+	}
+
+	return result
+}
+
+func DecToHex(dec int) string {
+	if dec < 10 && dec >= 0 {
+		return fmt.Sprintf("%d", dec)
+	}
+
+	switch dec {
+	case 10:
+		return "A"
+	case 11:
+		return "B"
+	case 12:
+		return "C"
+	case 13:
+		return "D"
+	case 14:
+		return "E"
+	case 15:
+		return "F"
+	default:
+		panic("value not allowed")
+	}
 }
