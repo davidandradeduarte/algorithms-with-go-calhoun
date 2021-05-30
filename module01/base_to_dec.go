55c2b3357cd01ef32fb4866c3a4cc476c027a100
David Duarte <davidandradeduarte@gmail.com>
2021-05-30 23:52:42 +0100

    adding base to decimal

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,5 +1,10 @@
 package module01
 
+import (
+	"math"
+	"strconv"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -9,5 +14,35 @@ package module01
 //   BaseToDec("1110", 2) => 14
 //
 func BaseToDec(value string, base int) int {
-	return 0
+	result := 0
+	i := len(value) - 1
+	for _, v := range value {
+		digit := HexToDec(string(v))
+		result += digit * int(math.Pow(float64(base), float64(i)))
+		i--
+	}
+	return result
+}
+
+func HexToDec(hex string) int {
+	if v, err := strconv.Atoi(hex); err == nil {
+		return v
+	}
+
+	switch hex {
+	case "A":
+		return 10
+	case "B":
+		return 11
+	case "C":
+		return 12
+	case "D":
+		return 13
+	case "E":
+		return 14
+	case "F":
+		return 15
+	default:
+		panic("value not allowed")
+	}
 }
