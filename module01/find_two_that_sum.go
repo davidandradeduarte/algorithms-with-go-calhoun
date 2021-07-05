a8c8a45c2735eeffbe8b3013451c41060328d310
David Duarte <davidandradeduarte@gmail.com>
2021-07-05 15:15:59 +0100

    adding find two that sum

--- module01/find_two_that_sum.go
+++ module01/find_two_that_sum.go
@@ -35,5 +35,15 @@ package module01
 //    for the sum of 4.
 //
 func FindTwoThatSum(numbers []int, sum int) (int, int) {
-	return 0, 0
+	for i, v := range numbers {
+		for j, nv := range numbers {
+			if j == i {
+				continue
+			}
+			if v+nv == sum {
+				return i, j
+			}
+		}
+	}
+	return -1, -1
 }
